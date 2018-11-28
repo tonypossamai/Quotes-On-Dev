3b9e02512acb7738134c301975fbc3a21e5e0188
Tony <tony_possamai@hotmail.com>
2018-11-28 11:14:39 +0000

    Styling responsive for tablet and desktop

--- footer.php
+++ footer.php
@@ -9,17 +9,18 @@
 
 			</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				
-				<div class="site-info">
-				<nav id="site-navigation" class="main-navigation menu-main-navigation-container" role="navigation">			
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					
+					<div class="site-info">
+					<nav id="site-navigation" class="main-navigation menu-main-navigation-container" role="navigation">			
 						<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>			
-				</nav><!-- #site-navigation -->
-					<p>Brought to you by <a href="<?php echo esc_url( 'https://github.com/tonypossamai' ); ?>"><?php the_author()?></a></p>
-				</div><!-- .site-info -->
+					</nav><!-- #site-navigation -->
+						<p>Brought to you by <a href="<?php echo esc_url( 'https://github.com/tonypossamai' ); ?>"><?php the_author()?></a></p>
+					</div><!-- .site-info -->
 
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+				</footer><!-- #colophon -->
+				
+			</div><!-- #page -->
 
 		<?php wp_footer(); ?>
 
